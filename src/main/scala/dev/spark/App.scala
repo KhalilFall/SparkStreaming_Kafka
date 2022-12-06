efe858ad5fb8b509e1233d3ebcc66367b6fac41c
Ibrahma Fall <ibrahima.fall@atos.net>
2022-12-06 00:07:43 +0000

    adding data

--- src/main/scala/dev/spark/App.scala
+++ src/main/scala/dev/spark/App.scala
@@ -1,8 +1,8 @@
-package com.sonatel.reporting
+package dev.spark
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.{StringType, StructType}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
 
 object App {
 
@@ -15,7 +15,7 @@ object App {
     System.setProperty("hadoop.home.dir","C:\\hadoop")
     val spark = SparkSession
       .builder
-      .master("local")
+      .master("local[*]")
       .appName("Exemple")
       .getOrCreate()
     //spark.conf.set("spark.hadoop.parquet.enable.summary-metadata", false)
@@ -27,21 +27,23 @@ object App {
       .readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("subscribe", "testTopic")
-      .option("startingOffsets","earliest")
-      .option("failOnDataLoss", "false")
+      .option("subscribe", "dataFimela")
+      .option("startingOffsets","latest") //earliest
       .load()
       .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
 
     val schema: StructType = new StructType()
-                        .add("date_cas", StringType, true)
+                        .add("date", StringType, true)
+                        .add("nb_cas", StringType, true)
                         .add("variant", StringType, true)
-                        .add("id_pays", StringType, true)
+                        .add("id_pays", IntegerType, true)
 
     val refoemated_df = kafkaDS.withColumn("struct_value", from_json(col("value"), schema))
     val explode_df = refoemated_df
-      .withColumn("variant", col("struct_value").getField("variant"))
-      .withColumn("id_pays", col("struct_value").getField("id_pays"))
+                        .withColumn("id_pays", col("struct_value").getField("id_pays"))
+                        .withColumn("nb_cas", col("struct_value").getField("nb_cas"))
+                        .withColumn("variant", col("struct_value").getField("variant"))
+                        .withColumn("date", col("struct_value").getField("date"))
 
     explode_df
       .writeStream
